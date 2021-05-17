708feb788b0780dd1e78531aea211fa8d06cebde
PHAGUN JAIN <PHAGUNJAIN@protonmail.com>
2021-05-17 17:39:19 +0530

    initial commit

---
+++ src/components/banner.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Banner extends Component {
+  render() {
+    return <img src="https://imgur.com/gallery/IzSX96i" alt="sample img" />;
+  }
+}
---
+++ src/components/form.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import Banner from "./banner";
+export default class Form extends Component {
+  render() {
+    return <Banner />;
+  }
+}
