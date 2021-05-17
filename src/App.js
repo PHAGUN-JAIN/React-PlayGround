606368e75d3c033d8d3ea827808cc057f366e464
PHAGUN JAIN <PHAGUNJAIN@protonmail.com>
2021-05-17 17:00:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./styles.css";
 import Counter from "./components/counnter";
+import Card from "./components/card";
 export default function App() {
   return (
     <div className="App">
       <Counter />
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Card />
     </div>
   );
 }
