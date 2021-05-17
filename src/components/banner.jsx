aa0d902bc5ab4260d3d9da133720433d661b6576
PHAGUN JAIN <PHAGUNJAIN@protonmail.com>
2021-05-17 17:46:11 +0530

    fixed img url

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 
 export default class Banner extends Component {
   render() {
-    return <img src="https://imgur.com/gallery/IzSX96i" alt="sample img" />;
+    return (
+      <img
+        src="https://cdn.pixabay.com/photo/2020/01/14/15/41/landscape-4765322_1280.jpg"
+        alt="sample img"
+      />
+    );
   }
 }
