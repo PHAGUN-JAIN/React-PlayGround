f11d645b6f977708ed83d37d68644b7c18a01983
PHAGUN JAIN <PHAGUNJAIN@protonmail.com>
2021-05-18 18:43:22 +0530

    initial commit

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,6 +2,29 @@ import React, { Component } from "react";
 import Banner from "./banner";
 export default class Form extends Component {
   render() {
-    return <Banner />;
+    return (
+      <div>
+        <Banner />
+        <h1>hi</h1>
+        <div>
+          <form action="">
+            <legend>registration</legend> <br /> <br />
+            <label htmlFor="name">NAME:</label> <br />
+            <input id="name" type="text" />
+            <br />
+            <br />
+            <label htmlFor="branch">BRANCH:</label> <br />
+            <input id="branch" type="text" />
+            <br />
+            <br />
+            <label htmlFor="sap">SAP ID:</label> <br />
+            <input id="sap" type="text" />
+            <br />
+            <br />
+            <input type="button" value="SUBMIT" />
+          </form>
+        </div>
+      </div>
+    );
   }
 }
