705aded6cab8c2f579fbac7a63ea50706efffd28
PHAGUN JAIN <PHAGUNJAIN@protonmail.com>
2021-05-20 14:57:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import "./styles.css";
-import Counter from "./components/counnter";
-import Card from "./components/card";
+
 import Form from "./components/form";
 export default function App() {
   return (
     <div className="App">
-      <Counter />
-      <Card />
       <Form />
     </div>
   );
